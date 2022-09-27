8d7fc34b07f771c2453eb24650b8ae3c364d4192
vysion <77179954+vysiondev@users.noreply.github.com>
2022-09-26 18:13:23 -0700

    - Optimize JS - Clean up CSS - Remove combo counter (redundant) - Use current beatmap background image as counter bg - Scroll text left to right if too long (map title)

--- README.md
+++ README.md
@@ -2,14 +2,13 @@
 
 PP counter/theme for [gosumemory](https://github.com/l3lackShark/gosumemory).
 
-![](https://media.discordapp.net/attachments/622937123338584074/1023550229070499903/unknown.png)
-
-![](https://media.discordapp.net/attachments/622937123338584074/1023550633791463506/unknown.png)
+![](https://media.discordapp.net/attachments/622937123338584074/1024118994636116018/unknown.png)
 
 Contains:
 
+- Beatmap background image
 - Circle map progress icon
-- Artist, song name and difficulty
+- Artist, song name and difficulty (scrolls right to left if too long)
 - Approximate star rating
 - Mods used
 - BPM
@@ -17,12 +16,7 @@ Contains:
 - Potential maximum PP value
 - 100/50/0 counts
 - Unstable rate
-- Current/maximum combo
 
 Customizing this is really easy, just go into style.css and change the root variables to different colors if you want.
 
 Best used for standard mode, but taiko and catch modes work fine (note that the star rating may be wildly inaccurate).
-
-The css will fuck up if combo exceeds 99999 or if the pp exceeds 9999.
-
-(this counter is 1150x90)
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const websocketURI = "ws://127.0.0.1:24050/ws";
+const baseTitleAnimTime = 7;
 
 const currentPP = document.getElementById("pp");
 const maximumPP = document.getElementById("pp-maximum-this-play");
-const currentCombo = document.getElementById("combo");
-const maximumCombo = document.getElementById("max-combo");
 const hit100 = document.getElementById("hit100");
 const hit50 = document.getElementById("hit50");
 const hit0 = document.getElementById("hit0");
@@ -13,6 +12,24 @@ const mapStarRating = document.getElementById("map-sr");
 const modsEnabled = document.getElementById("map-mods");
 const mapBPM = document.getElementById("map-bpm");
 const mapProgress = document.getElementById("map-progress");
+const bg = document.getElementById("bg");
+
+function updateMapTitle(newTitle) {
+    mapInfo.classList.remove("marquee");
+    mapInfo.innerHTML = newTitle;
+    void mapInfo.clientWidth;
+    const mapTitleWidth = getComputedStyle(document.documentElement).getPropertyValue('--map-title-width');
+    if (mapInfo.clientWidth >= mapTitleWidth) {
+        document.documentElement.style.setProperty('--animation-time', `${baseTitleAnimTime + (baseTitleAnimTime * ((mapInfo.clientWidth - mapTitleWidth) / mapTitleWidth))}s`);
+        document.documentElement.style.setProperty('--offset', `-${mapInfo.clientWidth}px`);
+        mapInfo.classList.add("marquee");
+    }
+}
+
+function clampMax(x, lim) {
+    if (x > lim) return lim;
+    return x;
+}
 
 const socket = new ReconnectingWebSocket(websocketURI);
 
@@ -34,10 +51,23 @@ let mods = "";
 let tempName = "";
 let tempDiff = "";
 let tempMapper = "";
+let tempSR = 0;
+
+let tempBg = "";
+
+let tempState = 0;
 
 socket.onmessage = (e) => {
     const json = JSON.parse(e.data);
-
+    if (tempState !== json.menu.state) {
+        tempState = json.menu.state;
+    }
+    if (tempBg !== json.menu.bm.path.full) {
+        tempBg = json.menu.bm.path.full;
+        let img = json.menu.bm.path.full.replace(/#/g, '%23').replace(/%/g, '%25');
+        bg.setAttribute("src", `http://127.0.0.1:24050/Songs/${img}?a=${Math.random()}`);
+        document.documentElement.style.setProperty('--bg-offset', `-${bg.clientHeight / 3}px`);
+    }
     if (json.menu.bm.metadata.title !== tempName || json.menu.bm.metadata.difficulty !== tempDiff || json.menu.bm.metadata.mapper !== tempMapper) {
         tempName = json.menu.bm.metadata.title;
         tempDiff = json.menu.bm.metadata.difficulty;
@@ -50,61 +80,34 @@ socket.onmessage = (e) => {
             }
             mapBPM.innerHTML = str;
         }
-
-        mapInfo.innerHTML = `${json.menu.bm.metadata.artist} - ${json.menu.bm.metadata.title} [${json.menu.bm.metadata.difficulty}]`
-        mapStarRating.innerHTML = json.menu.bm.stats.fullSR;
-
+        updateMapTitle(`${json.menu.bm.metadata.artist} - ${json.menu.bm.metadata.title} [${json.menu.bm.metadata.difficulty}]`);
+    }
+    if (json.menu.bm.stats['fullSR'] !== tempSR) {
+        tempSR = json.menu.bm.stats.fullSR;
+        mapStarRating.innerHTML = `~${json.menu.bm.stats.fullSR}`;
     }
     if (json.menu.mods.str !== mods) {
+        mods = json.menu.mods.str;
         modsEnabled.innerHTML = `+${json.menu.mods.str}`
     }
-    if (json.gameplay.pp.current !== '') {
+    if (tempState === 2 || tempState === 7 || tempState === 14) {
         currentPP.innerHTML = Math.round(json.gameplay.pp.current);
-    } else {
-        currentPP.innerHTML = 0;
-    }
-    if (json.gameplay.pp.maxThisPlay !== '') {
         maximumPP.innerHTML = Math.round(json.gameplay.pp.maxThisPlay);
-    } else {
-        maximumPP.innerHTML = 0;
-    }
-    if (json.gameplay.combo.current !== '') {
-        currentCombo.innerHTML = Math.round(json.gameplay.combo.current);
-    } else {
-        currentCombo.innerHTML = 0;
-    }
-    if (json.gameplay.combo.max !== '') {
-        maximumCombo.innerHTML = Math.round(json.gameplay.combo.max);
-    } else {
-        maximumCombo.innerHTML = 0;
-    }
-    if (json.gameplay.hits[100] > 0) {
         hit100.innerHTML = json.gameplay.hits[100];
-    } else {
-        hit100.innerHTML = 0;
-    }
-    if (json.gameplay.hits[50] > 0) {
         hit50.innerHTML = json.gameplay.hits[50];
-    } else {
-        hit50.innerHTML = 0;
-    }
-    if (json.gameplay.hits[0] > 0) {
         hit0.innerHTML = json.gameplay.hits[0];
-    } else {
-        hit0.innerHTML = 0;
-    }
-    if (json.gameplay.hits.unstableRate > 0) {
         unstableRate.innerHTML = Math.round(json.gameplay.hits.unstableRate);
     } else {
+        hit100.innerHTML = 0;
+        hit50.innerHTML = 0;
+        hit0.innerHTML = 0;
         unstableRate.innerHTML = 0;
+        currentPP.innerHTML = 0;
+        maximumPP.innerHTML = 0;
     }
-
-    if (json.menu.bm.time.current > 0 && json.menu.bm.time.full > 0) {
-        const val = (json.menu.bm.time.current / json.menu.bm.time.full);
-        if (val > 1.01) return;
-        mapProgress.setAttribute("style", `--value:${Math.floor(val * 100)}`);
+    if (json.menu.bm.time.current !== undefined && json.menu.bm.time.full !== undefined) {
+        mapProgress.setAttribute("style", `--value:${clampMax((json.menu.bm.time.current / json.menu.bm.time.full) * 100, 100)}`);
     } else {
         mapProgress.setAttribute("style", "--value:0");
     }
-
 }
